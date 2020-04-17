48edc57bc13cb4e5a79136552578c4bc95e4e497
Terkin Ivan <gwa_1978@mail.ru>
2020-04-17 15:58:01 +0300

    Canvas

---
+++ lab6/2_canvas.py
@@ -0,0 +1,18 @@
+from tkinter import *
+root=Tk() #создание
+c=Canvas(root, width=400, height=300, bg='white')
+c.pack()
+# c.create_line(20,20,150,150)
+# c.create_line(30,20,170,150, fill='green', width=5, arrow=LAST, dash=(10,2), activefill='lightgreen',arrowshape="10 20 10")
+# c.create_rectangle(50,50,100,100)
+# c.create_rectangle(150,150, 200,200, fill='orange',outline='black')
+# c.create_oval(10, 120, 190, 190, width=2)
+# c.create_rectangle(10, 120, 190, 190, width=2)
+# c.create_oval(10, 10, 190, 190, fill='lightgrey', outline='white')
+# c.create_arc(10, 10, 190, 190, start=0, extent=45, fill='red')
+# c.create_arc(10, 10, 190, 190, start=180, extent=25, fill='orange')
+# c.create_arc(10, 10, 190, 190, start=240, extent=100, style=CHORD, fill='green')
+# c.create_arc(10, 10, 190, 190, start=160, extent=-70, style=ARC, outline='darkblue', width=5)
+c.create_text(100, 100, text="Hello World,\nPython\nand Tk", justify=CENTER, font="Verdana 14")
+c.create_text(200, 200, text="About this", anchor=SE, fill="grey")
+root.mainloop()
